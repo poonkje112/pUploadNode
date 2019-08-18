b8f726c5a716ad4ffbba2488ed7e5d82ee955196
poonkje <aaron.knoop@live.nl>
2019-08-18 23:12:30 +0200

    Update ipintel-api.js

--- ipintel-api.js
+++ ipintel-api.js
@@ -5,6 +5,7 @@ function checkIP(ip) {
     request(`http://check.getipintel.net/check.php?ip=${ip}&contact=${info["contactEmail"]}&flags=b`,
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
+                console.log(body);
                 if (parseFloat(body) > 0.99) {
                     console.log(`ALERT: ${ip} Has been blocked!`)
                     return true;
